c7cd61aba5a6bee352eed210185ce4138742244a
TinuCharly <134133909+TinuCharly@users.noreply.github.com>
2025-02-24 14:28:12 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,13 @@ tl.from(paragraph, {
     duration: 1,
 });
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+    e.preventDefault();
+  }
+});
+
 
 const sections = ["about", "gallery", "features", "layout", "videos"];
 
